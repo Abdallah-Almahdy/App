1911f0f14e8d43faa408467c9e46d288a43e30b4
Abdallah-Almahdy <at3@fci.suezuni.edu.eg>
2025-04-24 00:42:39 +0200

    update profile

--- app/Models/adminProfile.php
+++ app/Models/adminProfile.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class adminProfile extends Model
+class AdminProfile extends Model
 {
     use HasFactory;
 
